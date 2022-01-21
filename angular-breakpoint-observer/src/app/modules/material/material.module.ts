76df9b60170bc8ede5164db81d330e4afcc2b0d6
Brian Wilson <wilsonedit@gmail.com>
2022-01-21 15:43:23 -0600

    setup basic nav bar

--- angular-breakpoint-observer/src/app/modules/material/material.module.ts
+++ angular-breakpoint-observer/src/app/modules/material/material.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
