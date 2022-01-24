9466cbb8b875bdb8df7b5ef43894c2fd4c3e893f
Brian Wilson <wilsonedit@gmail.com>
2022-01-24 09:02:34 -0600

    added mat-menu to header. no links within it just yet

--- angular-breakpoint-observer/src/app/modules/material/material.module.ts
+++ angular-breakpoint-observer/src/app/modules/material/material.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -11,12 +12,14 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule { }
