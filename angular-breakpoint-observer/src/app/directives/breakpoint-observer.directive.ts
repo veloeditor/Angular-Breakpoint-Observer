a1e851df647342a6ac475b402b3d67c04449bb32
Brian Wilson <wilsonedit@gmail.com>
2022-01-24 12:59:50 -0600

    added code to directive to look for breakpoints. Connected this to home component to show text PC, tablet or mobile based this directive

--- angular-breakpoint-observer/src/app/directives/breakpoint-observer.directive.ts
+++ angular-breakpoint-observer/src/app/directives/breakpoint-observer.directive.ts
@@ -1,10 +1,43 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
+
+type Size = "small" | "medium" | "large";
+
+const config = {
+  small: [Breakpoints.XSmall],
+  medium: [Breakpoints.Small, Breakpoints.Medium],
+  large: [Breakpoints.Large, Breakpoints.XLarge]
+}
 
 @Directive({
   selector: '[appBreakpointObserver]'
 })
-export class BreakpointObserverDirective {
+export class BreakpointObserverDirective implements OnDestroy {
+  private subscription = new Subscription();
 
-  constructor() { }
+  @Input("appBreakpointObserver") set size(value: Size) {
+    this.subscription.unsubscribe();
+    this.subscription = this.observer
+      .observe(config[value])
+      .subscribe(this.updateView);
+  }
+
+  constructor(
+    private observer: BreakpointObserver,
+    private vcRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) { }
+
+  updateView = ({ matches }: BreakpointState) => {
+    if (matches && !this.vcRef.length) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else if (!matches && this.vcRef.length) {
+      this.vcRef.clear();
+    }
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
