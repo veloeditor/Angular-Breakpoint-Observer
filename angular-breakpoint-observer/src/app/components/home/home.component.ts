991a36139cd8985c6686d545f562775e0a2cb210
Brian Wilson <wilsonedit@gmail.com>
2022-01-31 07:49:51 -0600

    injected the breakpoint service into the service component. only console.logging right now

--- angular-breakpoint-observer/src/app/components/home/home.component.ts
+++ angular-breakpoint-observer/src/app/components/home/home.component.ts
@@ -1,24 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { BreakpointObserverService } from 'src/app/services/breakpoint-observer.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   large: any = 'large';
   medium: any = 'medium';
   small: any = 'small';
   isMobile: boolean = false;
 
-  constructor(private breakpointObserverService: BreakpointObserverService) { }
-
-  ngOnInit(): void {
-    // once per component initiation. this will not dynamically check with each breakpoint change as currently setup
-    this.isMobile = this.breakpointObserverService.getBreakpointState();
-    console.log('isMobile:', this.isMobile);
-  }
+  constructor() { }
 
 }
--- angular-breakpoint-observer/src/app/components/service-example/service-example.component.ts
+++ angular-breakpoint-observer/src/app/components/service-example/service-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserverService } from 'src/app/services/breakpoint-observer.service';
 
 @Component({
   selector: 'app-service-example',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-example.component.scss']
 })
 export class ServiceExampleComponent implements OnInit {
+  isMobile: boolean = false;
 
-  constructor() { }
+  constructor(private breakpointObserverService: BreakpointObserverService) { }
 
   ngOnInit(): void {
+    // once per component initiation. this will not dynamically check with each breakpoint change as currently setup
+    this.isMobile = this.breakpointObserverService.getBreakpointState();
+    console.log('isMobile:', this.isMobile);
   }
 
 }
