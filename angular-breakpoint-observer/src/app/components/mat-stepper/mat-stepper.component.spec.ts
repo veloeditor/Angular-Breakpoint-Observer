022c83a0a4a4d62e8d24b8370e0c8c6aee52beb9
Brian Wilson <wilsonedit@gmail.com>
2022-02-01 14:58:45 -0600

    updated unit test for stepper to include coverage for breakpoint observer

--- angular-breakpoint-observer/src/app/components/mat-stepper/mat-stepper.component.spec.ts
+++ angular-breakpoint-observer/src/app/components/mat-stepper/mat-stepper.component.spec.ts
@@ -1,8 +1,8 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 import { StepperOrientation } from '@angular/material/stepper';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 
 import { MatStepperComponent } from './mat-stepper.component';
 
@@ -16,12 +16,32 @@ describe('MatStepperComponent', () => {
   beforeEach(async () => {
     formBuilderMock = new FormBuilder();
     stepObserverMock = new Observable<StepperOrientation>();
+    breakPointObserverMock = TestBed.inject(BreakpointObserver);
+
     component = new MatStepperComponent(breakPointObserverMock, formBuilderMock);
 
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should call the breakpointobserver when app is initiated and set isMobile to false when in desktop mode', () => {
+      const value = { matches: false, breakpoints: { '(max-width: 600px)': false } };
+      spyOn(breakPointObserverMock, 'observe').and.returnValue(of(value));
+      component.ngOnInit();
+      expect(breakPointObserverMock.observe).toHaveBeenCalled();
+      expect(component.isMobile).toBeFalse();
+    });
+
+    it('should return isMobile as true when breakpointObserver matches mobile width', () => {
+      const value = { matches: true, breakpoints: { '(max-width: 600px)': true } };
+      spyOn(breakPointObserverMock, 'observe').and.returnValue(of(value));
+      // spyOn(breakPointObserverMock.observe('(max-width: 600px)'), 'pipe').and.callThrough();
+      component.ngOnInit();
+      expect(component.isMobile).toBeTrue();
+    });
   });
 
 });
