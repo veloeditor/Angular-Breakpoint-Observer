5a77b0a0dff14aeeecbdb77215eed9d631847a7e
Brian Wilson <wilsonedit@gmail.com>
2022-02-01 10:13:03 -0600

    updated unit test

--- angular-breakpoint-observer/src/app/components/mat-stepper/mat-stepper.component.spec.ts
+++ angular-breakpoint-observer/src/app/components/mat-stepper/mat-stepper.component.spec.ts
@@ -1,5 +1,8 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
+import { StepperOrientation } from '@angular/material/stepper';
+import { Observable, of } from 'rxjs';
 
 import { MatStepperComponent } from './mat-stepper.component';
 
@@ -7,10 +10,12 @@ describe('MatStepperComponent', () => {
   let component: MatStepperComponent;
   let breakPointObserverMock: BreakpointObserver;
   let formBuilderMock: FormBuilder;
+  let stepObserverMock: Observable<StepperOrientation>;
 
 
-  beforeEach(() => {
+  beforeEach(async () => {
     formBuilderMock = new FormBuilder();
+    stepObserverMock = new Observable<StepperOrientation>();
     component = new MatStepperComponent(breakPointObserverMock, formBuilderMock);
 
   });
@@ -18,4 +23,5 @@ describe('MatStepperComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
